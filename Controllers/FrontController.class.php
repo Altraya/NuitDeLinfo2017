a88d2f78bd4122c3917ec4bc6a750fcf6e38d691
Josselin <josselin.spang@gmail.com>
2017-12-07 22:27:59 +0000

    start to add real mini router

--- Controllers/FrontController.class.php
+++ Controllers/FrontController.class.php
@@ -3,19 +3,19 @@
 require_once("HomeController.class.php");
 
 class FrontController {
-    public static function dispatch()
+    public static function dispatch($module)
     {
         
-        /*switch ($variable) {
-            case 'value':
-                // code...
+        switch ($module) {
+            case 'map':
+                $homeController = new HomeController();
+                $homeController->signup();
                 break;
-            
-            default:
-                // code...
+            default: //home and default
+                $homeController = new HomeController();
+                $homeController->home();
                 break;
-        }*/
-        $homeController = new HomeController();
-        $homeController->signup();
+        }
+        
     }
 }
